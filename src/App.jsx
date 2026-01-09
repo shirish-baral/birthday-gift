3991411f993bf2ebd75869856bd7b03819c7494d
shirishbaral <baral.sirish7@gmail.com>
2026-01-10 01:21:03 +0530

    Delete App.jsx

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import BirthdayGame from './components/BirthdayGame';
-
-export default function App() {
-  return (
-    <div className="w-full h-screen bg-gradient-to-br from-pink-100 to-purple-100">
-      <BirthdayGame recipientName="[HER NAME]" />
-    </div>
-  );
-}
