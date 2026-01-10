4f93392a8fa75f87a86689365548959bec5d8739
shirishbaral <baral.sirish7@gmail.com>
2026-01-10 08:52:35 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import BirthdayGame from './components/BirthdayGame';
+
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-br from-pink-100 to-purple-100">
+      <BirthdayGame recipientName="[HER NAME]" />
+    </div>
+  );
+}
